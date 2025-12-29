290b61db904d16dfc5502a528a6599b45ab0a86d
Abibullayev Azizbek Oybekovich <abibullayev17@Abibullayevs-MacBook-Pro.local>
2025-12-29 20:23:02 +0500

    added type_conversion.cpp

---
+++ type_conversion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* 
+        Type conversion in C++ is when one data type is converted to another.
+        There are two types of type conversion in C++:
+        1) Implicit conversion — happens automatically
+        2) Explicit conversion — done by the programmer
+    */
+
+    /*
+    int pi = 3.14;
+
+    cout << pi; // => 3 => implicit
+
+    */
+    double pi = (int) 3.14; // => explicit
+    cout << pi << '\n';
+    
+    return 0;
+}
