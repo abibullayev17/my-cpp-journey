71f0eea43228a3ea11fe80d57f974243bb31d587
abibullayev17 <abibullayev17@gmail.com>
2025-12-30 00:49:08 +0500

    added ternary_operator.cpp

---
+++ ternary_operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+    // ternary operator is a replacement for if else statement
+
+    double GPA = 3.8;
+
+    GPA >= 3.8 ? cout << "Good" : cout << "Bad";
+
+    return 0;
+}
