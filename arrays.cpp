94d702087b8a8bdd9137ca24b037ea205e23a15f
abibullayev17 <abibullayev17@gmail.com>
2026-01-05 03:57:47 +0500

    added arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // array = a data structure that can hold multiple values 
+    // values can be accessed by an index number
+    // in order to assign values to an array later, we have to assign the size first
+    
+    std::string car[] = {"Corvette", "Chevrolet", "Toyota"};
+
+    std::cout << car[0];
+
+    return 0;
+}
